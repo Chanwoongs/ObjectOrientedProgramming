55d1853877866d5dd37e1b401db5db33de6429d2
Chanwoong Bang <bck4044@gmail.com>
2025-02-11 15:35:48 +0900

    feat: 소방관과 관리자를 모두 포함하는 소방서 클래스를 생성, 모든 출근 직원을 우선 void*로 받았지만 이름을 참조하려하니 마땅한 방법이 없음. reinterpret_cast를 하는 방법밖에 없음.

---
+++ 2.FireStation/FireStation.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <vector>
+
+class FireStation
+{
+public:
+    // 출근 함수.
+    void ClockIn(void* staffMember)
+    {
+        // 이미 출근했는지 확인.
+        bool contains = false;
+
+        for (void* staff : clockedInStaff)
+        {
+            if (staff == staffMember)
+            {
+                contains = true;
+                break;
+            }
+        }
+
+        // 출근 안했을 때 배열에 추가.
+        if (!contains)
+        {
+            clockedInStaff.emplace_back(staffMember);
+        }
+    }
+
+    // 출석 확인 함수.
+    void RollCall()
+    {
+        for (void* staff : clockedInStaff)
+        {
+            // std::cout << staff->???
+        }
+    }
+
+private:
+    // 직원 관리 변수.
+    std::vector<void*> clockedInStaff;
+};
