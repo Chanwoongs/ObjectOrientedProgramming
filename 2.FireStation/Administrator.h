712a197c7933b7dc3d9a378b044f97121b3bb48c
Chanwoong Bang <bck4044@gmail.com>
2025-02-11 15:29:47 +0900

    feat: 관리자 클래스

---
+++ 2.FireStation/Administrator.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <string>
+
+class Administrator
+{
+public:
+    Administrator(const std::string& title, const std::string& firstName, const std::string& lastName)
+        : title(title), firstName(firstName), lastName(lastName)
+    {
+
+    }
+
+    // Getter.
+    std::string GetTitle() const{ return title; }
+    std::string GetFirstName() const{ return firstName; }
+    std::string GetLastName() const { return lastName; }
+
+    std::string GetName() const { return title + " " + lastName + " " + firstName; }
+
+private:
+    std::string title;
+    std::string firstName;
+    std::string lastName;
+};
