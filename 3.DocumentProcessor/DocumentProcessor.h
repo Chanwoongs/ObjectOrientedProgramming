3b1017236212fb12e4e7de3551a12dac8d944311
Chanwoong Bang <bck4044@gmail.com>
2025-02-13 11:46:07 +0900

    feat: 문서 처리 하는 과정에 대한 클래스를 만들어 문서만 인자로 받아 모든 과정을 처리

---
+++ 3.DocumentProcessor/DocumentProcessor.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "DocumentProcess.h"
+
+class DocumentProcessor
+{
+public:
+    static void Process(const Document& document)
+    {
+        DocumentProcess::TranslateIntoFrench(document);
+        DocumentProcess::SpellCheck(document);
+        DocumentProcess::Repaginate(document);
+    }
+};
--- 3.DocumentProcessor/main.cpp
+++ 3.DocumentProcessor/main.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
 
+#include "DocumentProcessor.h"
+
 int main()
 {
+    // 문서 객체 생성.
+    Document doc1("Mo Salah", "2025.02.13", "C++ Programming");
+    Document doc2("Heungmin Son", "2005.10.09", "Android");
 
+    // 문서 처리.
+    std::cout << "문서1 처리\n";
+    DocumentProcessor::Process(doc1);
+    std::cout << '\n';
+
+    std::cout << "문서2 처리\n";
+    DocumentProcessor::Process(doc2);
+    std::cout << '\n';
 }
